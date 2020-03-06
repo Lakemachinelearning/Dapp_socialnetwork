ae51780ace5c2fd769c94cea1bfda3fdb98ab1fe
Lake Developer <lakeinvestmentsprime@gmail.com>
2020-03-07 00:51:16 +0200

    Tuned SocialNetwork.sol + test

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -1,9 +1,39 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.21 <0.6.0;
 
 contract SocialNetwork {
   string public name;
+  uint public postCount = 0;
+  mapping(uint => Post) public posts;
+
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address author;
+  }
+
+  event PostCreated(
+    uint id,
+    string content,
+    uint tipAmount,
+    address author
+  );
 
   constructor() public {
     name = "Lake machinelearning Dapp";
   }
+
+  function createPost(string memory _content) public {
+    // Require valid _content
+    require(bytes(_content).length > 0);
+    // Increment the post count
+    postCount ++;
+    // Create the post
+    posts[postCount] = Post(postCount, _content, 0, msg.sender);
+    // Trigger event
+    emit PostCreated(postCount, _content, 0, msg.sender);
+  }
+
+
+
 }
