2dbb33b375957887fc816787aa7571586f54fce5
Lake Developer <lakeinvestmentsprime@gmail.com>
2020-03-08 21:26:59 +0200

    client development

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,71 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import logo from '../read-thum.png';
 import './App.css';
+import Web3 from 'web3';
+import Navbar from './Navbar';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    // takes connection from metamask
+    // wires it up to WEB3. reads it inside web3 connection inside our app
+    // looks ethereum provider inside your window
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    // if it does not have one. = creates new one with web3
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying Metamask! :) ')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts);
+    this.setState({ account: accounts[0] })
+  }
+
+  // props = properties for the component
+  // and we want to call super on this constructor
+  // this.state = {}     <---- this is what state object looks like
+  // it is just an object. + has a value in it (account: '')
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Lake Machinelearning
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
+
+        <Navbar account = { this.state.account } />
+
+        <div container-fluid mt-5>
+          <div className = "row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
+                <br></br>
+                <br></br>
+                <h1> Lake Machinelearning </h1>
+                <a href="http://www.rate.ee">
+                  <img src = {logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Lake Machinelearning Dapp</h1>
-
-                <a
-                  className="App-link"
-                  href="http://www.google.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
+             </div>
+          </main>
           </div>
         </div>
       </div>
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -29,9 +29,12 @@
     describe('posts', async () => {
       let result, postCount
 
-      it('creates posts', async () => {
+      before( async () => {
         result = await socialNetwork.createPost('This is my first post', { from: author })
         postCount = await socialNetwork.postCount()
+      })
+
+      it('creates posts', async () => {
         // SUCCESS
         assert.equal(postCount, 1)
         const event = result.logs[0].args
@@ -46,11 +49,44 @@
       })
 
       it('lists posts',  async () => {
-        // TODO: Fill me in
+        const post = await socialNetwork.posts(postCount)
+        assert.equal(post.id.toNumber(), postCount.toNumber(), 'id is correct')
+        assert.equal(post.content, 'This is my first post', 'content is correct')
+        assert.equal(post.tipAmount, '0', 'tip amount is correct')
+        assert.equal(post.author, author, 'author is correct')
       })
 
       it('allows users to tip posts', async () => {
-        // TODO: Fill me in
+
+        // Track the author balance before purchase
+        let oldAuthorBalance
+        oldAuthorBalance = await web3.eth.getBalance(author)
+        oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
+        result = await socialNetwork.tipPost(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+        // SUCCESS
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+        assert.equal(event.content, 'This is my first post', 'content is correct')
+        assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
+        assert.equal(event.author, author, 'author is correct')
+
+        // Check that author received funds
+        let newAuthorBalance
+        newAuthorBalance = await web3.eth.getBalance(author)
+        newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+        let tipAmount
+        tipAmount = web3.utils.toWei('1', 'Ether')
+        tipAmount = new web3.utils.BN(tipAmount)
+
+        const expectedBalance = oldAuthorBalance.add(tipAmount)
+
+        assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+        // Failure: Tries to tip a post that does not exist
+        await socialNetwork.tipPost(99, { from: tipper, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
       })
     })
 
